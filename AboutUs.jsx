9c733c0d55dff3d225a9f04a6756110874ac24a2
sultann-sultan <syedaayesha262@gmail.com>
2026-05-01 15:18:49 +0500

    Update AboutUs.jsx

--- AboutUs.jsx
+++ AboutUs.jsx
@@ -1,3 +1,8 @@
 export default function AboutUs() {
-  return <h1>Paradise Nursery - Plant Shop</h1>;
+  return (
+    <div>
+      <h1>Paradise Nursery</h1>
+      <p>We provide beautiful indoor plants for your home and office.</p>
+    </div>
+  );
 }
